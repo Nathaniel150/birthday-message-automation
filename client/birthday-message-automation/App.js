8759a46f3af3849e9ee8768d7a2c0f806e15b876
Snazzwaggler <kymkymlarson@gmail.com>
2023-03-24 21:47:53 -0600

    Added navigation

--- client/birthday-message-automation/App.js
+++ client/birthday-message-automation/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../birthday-message-automation/components/homeScreen';
+import Header from '../birthday-message-automation/components/header';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Hello this the birthday text automation app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          // options={{title: 'Welcome'}}
+        />
+        <Stack.Screen
+          name="Header"
+          component={Header}
+          // options={{title: 'Welcome'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ client/birthday-message-automation/components/homeScreen.js
@@ -0,0 +1,83 @@
+import { ImageBackground, Image, StyleSheet, Text, View, Pressable } from "react-native";
+
+
+export default function HomeScreen({ navigation }) {
+
+  return (
+    <View style={styles.container}>
+
+      <Pressable 
+        style={styles.container}
+        onPress={() => navigation.navigate("Header")}
+      >
+
+      <ImageBackground
+        resizeMode="cover"
+        style={styles.background}
+      >
+
+        <Image
+          source={require("../assets/favicon.png")}
+          style={styles.image}
+        />
+
+        <Text
+          style={styles.title}
+        >
+          Birthday Wishes
+        </Text>
+
+        <Text
+          style={styles.content}
+        >
+          boosts productivity and friendship
+        </Text>
+
+      </ImageBackground>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    // borderWidth: 5,
+    // borderColor: "blue",
+
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fffeee",
+
+    paddingBottom: 130,
+  },
+  image: {
+    // borderWidth: 5,
+    // borderColor: "pink",
+
+    flex: 1,
+    justifyContent: "flex-end",
+    height: 200,
+    maxHeight: 200,
+    width: 200,
+  },
+  title: {
+    // borderWidth: 5,
+    // borderColor: "orange",
+
+    flex: 0,
+    height: "auto",
+    textAlign: "center",
+    fontSize: 30,
+    
+  },
+  content: {
+    flex: 0,
+    height: "auto",
+    textAlign: "center",
+    fontSize: 15,
+  }
+});
