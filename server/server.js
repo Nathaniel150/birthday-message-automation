659467d31b2a927911a0af0e8d60dbc389c54b13
Nathaniel Poulson <jnp2000@byu.edu>
2023-03-24 19:05:11 -0600

    Set up client and server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,12 @@
-const express = require('express');
-const bodyParser = require("body-parser");
-
+const express = require("express");
 const app = express();
+const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-app.listen(3000);
-
-
-app.post('/', async (req, res) => {
-
-    res.sendStatus(200);
-
+app.get("/", (req, res) => {
+  console.log("Hit the endpoint");
+  res.send("Hello World!");
 });
 
-// app.delete('/api/items/:id', async (req, res) => {
-// to get parameters: req.params.id ^^
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
